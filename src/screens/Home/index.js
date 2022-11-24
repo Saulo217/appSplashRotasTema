ca1078f1a9ea04d8142299ad456cd0e34e05d82b
lab2@lab2.com <suporte@PC43.ACADEMICO>
2022-11-24 11:58:13 -0300

    added Carousel

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,11 +1,52 @@
-import { View, Image } from 'react-native';
-
-import img from './../../assets/cyberpunk2.jpg';
+import { View, Image, Text, StyleSheet } from 'react-native';
+import Carousel from 'react-native-snap-carousel';
 
 export function Home() {
+    const items = [
+        { title: 'img', img: 'https://i.pinimg.com/originals/b3/e6/c4/b3e6c4e162d5529f2ac47714b76073b5.gif' }
+    ];
+
+    function render({ item }) {
+        return (
+            <View style={stl.carouselItemContainer}>
+                <Text style={stl.carouselItemTitle}>{item.title}</Text>
+                <Image style={stl.carouselItemImage} source={{ uri: `${item.img}`}}></Image>
+            </View>
+        );
+    }
+
     return (
         <View style={{ flex: 1, justifyContent: 'center' }}>
-            <Image style={{ width: 300, height: 300, alignSelf: 'center' }}  source={img}/>
+            <Text style={{ alignSelf: 'center', fontSize: 20, fontWeight: 'bold' }}>Pixel Art</Text>
+            <Carousel
+               layout={'tinder'}
+               layoutCardOffset={10}
+               data={items}
+               sliderWidth={300}
+               itemWidth={600}
+               renderItem={render}
+               style={{ paddingBottom: 0}} 
+            ></Carousel>
         </View>
     );
 }
+
+const stl = StyleSheet.create({
+    carouselItemContainer: {
+        backgroundColor: '#9e9e9d',
+        borderRadius: 4,
+        height: 300,
+        padding: 10,
+        marginTop: 20
+    },
+    carouselItemTitle: {
+        fontSize: 21,
+        fontWeight: 'bold',
+        color: '#FFF'
+    },
+    carouselItemImage: {
+        width: '100%',
+        height: 200,
+        borderRadius: 4
+    }
+});
