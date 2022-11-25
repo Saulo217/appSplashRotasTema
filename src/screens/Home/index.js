e20f876eb6af73bd66e8d17ed688f11a430668a2
lab2@lab2.com <suporte@PC43.ACADEMICO>
2022-11-25 15:25:50 -0300

    added items

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -3,7 +3,12 @@ import Carousel from 'react-native-snap-carousel';
 
 export function Home() {
     const items = [
-        { title: 'img', img: 'https://i.pinimg.com/originals/b3/e6/c4/b3e6c4e162d5529f2ac47714b76073b5.gif' }
+        { title: 'Shinobi', img: 'https://i.pinimg.com/originals/b3/e6/c4/b3e6c4e162d5529f2ac47714b76073b5.gif' },
+        { title: 'Laundry', img: 'https://www.jbchost.com.br/madeinjapan/wp-content/uploads/2015/09/pixelart_tumblr_1041uuu_laundry.gif' },
+        { title: 'Girl', img: 'https://cdn.mos.cms.futurecdn.net/c9EFa7HUU2EoqtFHV7f8A9.gif' },
+        { title: 'Mario Lofi', img: 'https://www.themasterpicks.com/wp-content/uploads/2020/04/22b22287602523.5dbd29081561d.gif' },
+        { title: 'Mage', img: 'https://gd-hbimg.huaban.com/56f0af9d332d10b8070c06185b62801581d8bbb11491f-SuCpvZ' },
+
     ];
 
     function render({ item }) {
@@ -16,25 +21,40 @@ export function Home() {
     }
 
     return (
-        <View style={{ flex: 1, justifyContent: 'center' }}>
-            <Text style={{ alignSelf: 'center', fontSize: 20, fontWeight: 'bold' }}>Pixel Art</Text>
+        <View style={{ flex: 1,  alignItems: 'center', justifyContent: 'center', padding: 50, backgroundColor: '#04a6cf'}}>
+            <Text style={{ alignSelf: 'center', fontSize: 20, fontWeight: 'bold', textDecorationLine: 'underline'}}>Pixel Art</Text>
+            <Text style={{ fontSize: 20, fontWeight: 'bold', color: '', marginTop: 20}}>
+                O pixel art, ou arte pixel,
+                é uma forma de arte digital na qual as imagens são criadas 
+                ou editadas tendo como elemento básico os pixels.
+            </Text>
+            <Text style={{ backgroundColor: '#F05740', fontWeight: 'bold', marginTop: 15 }}> Nota: Pixel é a menor unidade em um tela </Text>
+            <Image source={{ uri: 'https://w7.pngwing.com/pngs/5/1014/png-transparent-undertale-pixel-art-minecraft-pixel-text-art-game-art.png' }}/>
             <Carousel
                layout={'tinder'}
                layoutCardOffset={10}
                data={items}
                sliderWidth={300}
-               itemWidth={600}
+               itemWidth={300}
                renderItem={render}
-               style={{ paddingBottom: 0}} 
+               style={{ paddingBottom: 0 }} 
             ></Carousel>
         </View>
     );
 }
 
 const stl = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
     carouselItemContainer: {
-        backgroundColor: '#9e9e9d',
+        backgroundColor: '#FF6529',
         borderRadius: 4,
+        borderWidth: 2,
+        borderColor: '#000', 
         height: 300,
         padding: 10,
         marginTop: 20
